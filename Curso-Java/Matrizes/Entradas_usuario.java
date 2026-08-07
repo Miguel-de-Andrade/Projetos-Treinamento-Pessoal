2870b9fadab81310efcb6ef609bc707473cd5f86
MigzTe <migueldeandradeee@gmail.com>
2026-08-07 10:23:40 -0300

    Exercicio do usuario digitar o valor da matriz

---
+++ Curso-Java/Matrizes/Entradas_usuario.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class Entradas_usuario {
+    public static void main(String[] args) {
+
+        int [] [] matriz = new int[2][2];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o valor da " +
+                        "linha " +i+ ", coluna " +j));
+            }
+        }
+
+        String matrizFeita = "A matriz ficou: \n";
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matrizFeita += matriz[i] [j] + " ";
+            }
+            matrizFeita += "\n";
+        }
+
+        JOptionPane.showMessageDialog(null,matrizFeita);
+
+    }
+}
