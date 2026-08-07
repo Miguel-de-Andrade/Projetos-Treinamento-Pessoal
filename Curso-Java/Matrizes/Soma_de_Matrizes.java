46c8ee7b6873673cf1a5f6954014c7c9005d338c
MigzTe <migueldeandradeee@gmail.com>
2026-08-07 11:06:49 -0300

    Exercício 02 adicionado: soma de matrizes com dimensões que o usuario pode escolher

---
+++ Curso-Java/Matrizes/Soma_de_Matrizes.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class Soma_de_Matrizes {
+    public static void main(String[] args) {
+
+        int linhas = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a quantidade de linha que as duas matrizes vão ter"));
+        int colunas = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a quantidade de colunas que as duas matrizes vão ter"));
+
+        int [] [] matrizA = new int[linhas][colunas];
+        int [] [] matrizB = new int[linhas][colunas];
+        int [] [] matrizResultante = new int [linhas][colunas];
+
+        for (int i = 0; i < matrizA.length; i++) {
+            for (int j = 0; j < matrizA[i].length; j++) {
+                matrizA[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o valor da " +
+                        "linha " +i+ ", coluna " +j+ " da primeira matriz"));
+            }
+        }
+
+        for (int i = 0; i < matrizB.length; i++) {
+            for (int j = 0; j < matrizB[i].length; j++) {
+                matrizB[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o valor da " +
+                        "linha " +i+ ", coluna " +j+ " da segunda matriz"));
+            }
+        }
+
+        String mensagem = "A matriz resultante do soma dessas duas é: \n";
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                matrizResultante[i][j] = matrizA[i][j] + matrizB[i][j];
+                mensagem += matrizResultante[i][j] + " ";
+            }
+            mensagem += "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, mensagem);
+    }
+}
